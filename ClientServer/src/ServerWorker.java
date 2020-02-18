7ba6f04b62316cee1ceec5d7467ff4c76b92070d
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2020-02-18 09:52:56 +0100

    Login + online status
    Slutning af part 2
    - Mangler stadigvæk chat

---
+++ ClientServer/src/ServerWorker.java
@@ -0,0 +1,115 @@
+import org.apache.commons.lang3.StringUtils;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.List;
+
+public class ServerWorker extends Thread {
+
+    private Socket clientSocket;
+    private String login = null;
+    private Server server;
+    private OutputStream outputStream;
+
+    public ServerWorker(Server server, Socket clientSocket) {
+        this.server = server;
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            handleClientSocket();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void handleClientSocket() throws IOException, InterruptedException {
+        InputStream inputStream = clientSocket.getInputStream();
+        this.outputStream = clientSocket.getOutputStream();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ( (line = reader.readLine()) != null) {
+            String[] tokens = StringUtils.split(line);
+            if (tokens != null && tokens.length > 0) {
+                String cmd = tokens[0];
+                if ("logoff".equals(cmd) || "quit".equalsIgnoreCase(cmd)) {
+                    handleLogoff();
+                    break;
+                } else if ("login".equalsIgnoreCase(cmd)) {
+                    handleLogin(outputStream, tokens);
+                } else {
+                    String msg = "Ukendt: " + cmd + "\n\r";
+                    outputStream.write(msg.getBytes());
+                }
+                //String msg = "You typed: " + line + "\n";
+                //outputStream.write(msg.getBytes());
+            }
+        }
+        clientSocket.close();
+    }
+
+    private void handleLogoff() throws IOException {
+        List<ServerWorker> workerList = server.getWorkerList();
+
+        // send other online user current user's status
+        String onlineMsg = login + " gik offline \n\r";
+        for (ServerWorker worker : workerList) {
+            if (!login.equals(worker.getLogin())) {
+                worker.send(onlineMsg);
+            }
+        }
+        clientSocket.close();
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    private void handleLogin(OutputStream outputStream, String[] tokens) throws IOException {
+        if (tokens.length == 3) {
+
+            String login = tokens[1];
+            String password = tokens[2];
+
+            if ((login.equals("guest") && password.equals("guest")) || (login.equals("jim") && password.equals("fisk"))) {
+                String msg = "Godkendt login!\n\r";
+                outputStream.write(msg.getBytes());
+                this.login = login;
+                System.out.println("User logged in successfully " + login);
+
+                List<ServerWorker> workerList = server.getWorkerList();
+
+                // send current user all other online logins
+                send("Brugere online: \n\r");
+                for (ServerWorker worker : workerList) {
+                    if (worker.getLogin() != null) {
+                        if (!login.equals(worker.getLogin())) {
+                            String msg2 = worker.getLogin() + "\n\r";
+                            send(msg2);
+                        }
+                    }
+                }
+
+                // send other online user current user's status
+                String onlineMsg = login + "\n\r";
+                for (ServerWorker worker : workerList) {
+                    if (!login.equals(worker.getLogin())) {
+                        worker.send(onlineMsg);
+                    }
+                }
+            } else {
+                String msg = "Fejl login\n\r";
+                outputStream.write((msg.getBytes()));
+            }
+        }
+    }
+    private void send(String msg) throws IOException {
+        if (login != null) {
+            outputStream.write(msg.getBytes());
+        }
+    }
+}
