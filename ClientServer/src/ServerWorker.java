297064ed5517012c65aa2087d7639b6d4f0e43ef
Andreas Brodersen <andreas.p.brodersen@gmail.com>
2020-02-24 13:28:11 +0100

    gruppe beskeder tilføjet
    part 3 færdig - skal nu til at lave protokollerne fra opgaven

--- ClientServer/src/ServerWorker.java
+++ ClientServer/src/ServerWorker.java
@@ -2,6 +2,8 @@ import org.apache.commons.lang3.StringUtils;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class ServerWorker extends Thread {
@@ -10,6 +12,7 @@ public class ServerWorker extends Thread {
     private String login = null;
     private Server server;
     private OutputStream outputStream;
+    private HashSet<String> topicSet = new HashSet<>();
 
     public ServerWorker(Server server, Socket clientSocket) {
         this.server = server;
@@ -30,22 +33,27 @@ public class ServerWorker extends Thread {
         InputStream inputStream = clientSocket.getInputStream();
         this.outputStream = clientSocket.getOutputStream();
 
+        outputStream.write(("\n\rVelkommen til chatten\n\rCommands: login, msg, join, leave, quit\n\n\r").getBytes());
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
         while ( (line = reader.readLine()) != null) {
             String[] tokens = StringUtils.split(line);
             if (tokens != null && tokens.length > 0) {
                 String cmd = tokens[0];
-                if ("logoff".equals(cmd) || "quit".equalsIgnoreCase(cmd)) {
+                if ("logoff".equals(cmd) || "logout".equals(cmd) || "quit".equalsIgnoreCase(cmd)) {
                     handleLogoff();
                     break;
                 } else if ("login".equalsIgnoreCase(cmd)) {
                     handleLogin(outputStream, tokens);
                 } else if ("msg".equalsIgnoreCase(cmd)) {
-                    handleMessage(tokens);
-                }
-                
-                else {
+                    String[] tokensMsg = StringUtils.split(line, null, 3);
+                    handleMessage(tokensMsg);
+                } else if ("join".equalsIgnoreCase(cmd)) {
+                    handleJoin(tokens);
+                } else if ("leave".equalsIgnoreCase(cmd)) {
+                    handleLeave(tokens);
+                } else {
                     String msg = "Ukendt: " + cmd + "\n\r";
                     outputStream.write(msg.getBytes());
                 }
@@ -56,18 +64,45 @@ public class ServerWorker extends Thread {
         clientSocket.close();
     }
 
-    // format: "msg" "login" msg
+    private void handleLeave(String[] tokens) {
+        if (tokens.length > 1) {
+            String topic = tokens[1];
+            topicSet.remove(topic);
+        }
+    }
+
+    public boolean isMemberOfTopic(String topic) {
+        return topicSet.contains(topic);
+    }
+
+    private void handleJoin(String[] tokens) {
+        if (tokens.length > 1) {
+            String topic = tokens[1];
+            topicSet.add(topic);
+        }
+    }
+
+    // format: "msg" "login" tekst...
+    // format: "msg" "#topic" tekst...
     private void handleMessage(String[] tokens) throws IOException {
-        String sentTo = tokens[1];
+        String sendTo = tokens[1];
         String body = tokens[2];
+
+        boolean isTopic = sendTo.charAt(0) == '#';
 
         List<ServerWorker> workerList = server.getWorkerList();
         for (ServerWorker worker : workerList) {
-            if (sentTo.equalsIgnoreCase(worker.getLogin())) {
-                String outMsg = "msg " + login + " " + body + "\n\r";
-                worker.send(outMsg);
+            if (isTopic) {
+                if (worker.isMemberOfTopic(sendTo)) {
+                    String outMsg = "msg " + sendTo + ":" + login + " " + body + "\n\r";
+                    worker.send(outMsg);
+                }
+            } else {
+                if (sendTo.equalsIgnoreCase(worker.getLogin())) {
+                    String outMsg = "msg " + login + " " + body + "\n\r";
+                    worker.send(outMsg);
+                }
             }
-
         }
     }
 
